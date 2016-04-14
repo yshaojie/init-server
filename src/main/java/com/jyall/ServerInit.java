8b109e4f12f78ca1582cc15b281781dcfa8025a7
shaojieyue <yuesj@huatu.com>
2016-04-14 17:56:01 +0800

    初始化java服务小程序

---
+++ src/main/java/com/jyall/ServerInit.java
@@ -0,0 +1,110 @@
+package com.jyall;
+
+import com.samskivert.mustache.Mustache;
+import com.samskivert.mustache.Template;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by shaojieyue on 9/13/15.
+ */
+public class ServerInit {
+    public static final String LOG_ROOT = "/data/logs/";
+    public static final String SERVER_ROOT = "/servers/";
+    public static void main(String[] args) throws IOException {
+        if (args == null || args.length<1) {
+            System.err.println("please set zhe config file.");
+            System.exit(1);
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            System.err.println("file["+file.getAbsolutePath()+"] not exist.");
+            System.exit(1);
+        }
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.err.println("file["+file.getAbsolutePath()+"] not exist.");
+            System.exit(1);
+        }
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        String server_args= StringUtils.trimToEmpty(properties.getProperty("server_args"));
+        String server_jvm_args= StringUtils.trimToNull(properties.getProperty("server_jvm_args"));
+        String server_name= StringUtils.trimToNull(properties.getProperty("server_name"));
+        String server_main_class= StringUtils.trimToNull(properties.getProperty("server_main_class"));
+        boolean valid = true;
+        if (server_jvm_args == null) {
+            System.err.println("property server_jvm_args not exist.");
+            valid = false;
+        }
+        if (server_name == null) {
+            System.err.println("property server_name not exist.");
+            valid = false;
+        }
+        if (server_main_class == null) {
+            System.err.println("property server_main_class not exist.");
+            valid = false;
+        }
+
+        if (!valid) {//参数检测不通过
+            System.exit(1);
+        }
+
+        //jvm 参数不包含-server的话,强制添加该参数
+        if (!server_jvm_args.contains("-server")) {
+            server_jvm_args = "-server "+server_jvm_args;
+        }
+
+        String server_home = SERVER_ROOT+server_name;
+        String server_log_home = LOG_ROOT+server_name;
+        File logHome = new File(server_log_home);
+        if (!logHome.exists()) {
+            logHome.mkdirs();
+            System.out.println("create dir:" + logHome.getAbsolutePath());
+        }
+        File serverHome = new File(server_home);
+        if (!serverHome.exists()) {
+            serverHome.mkdirs();
+            System.out.println("create dir:" + serverHome.getAbsolutePath());
+        }
+
+        Map params = new HashMap();
+        params.put("server_args",server_args);
+        params.put("server_jvm_args",server_jvm_args);
+        params.put("server_name",server_name);
+        params.put("server_main_class",server_main_class);
+        params.put("server_home",server_home);
+        params.put("server_log_home", server_log_home);
+        exec(params);
+    }
+
+    public static final void exec(Map<String,String> params) throws IOException {
+        final InputStream resourceAsStream = ServerInit.class.getClassLoader().getResourceAsStream("server_template.sh");
+        Reader source = new InputStreamReader(resourceAsStream);
+        final String server_home = params.get("server_home");
+        final String server_name = params.get("server_name");
+        File shellFile = new File(server_home +"/server.sh");
+        if (shellFile.exists()) {
+            System.err.println(params.get("server_name")+" 已经初始化.");
+            System.exit(1);
+        }
+        //创建文件
+        FileUtils.touch(shellFile);
+        shellFile.setExecutable(true);//设置shell脚本可执行
+        final Template compile = Mustache.compiler().compile(source);
+        final FileWriter fileWriter = new FileWriter(shellFile);
+        compile.execute(params, fileWriter);
+        fileWriter.flush();
+        IOUtils.closeQuietly(fileWriter);
+        System.out.println("init server ["+server_name+"] succcess");
+    }
+}
---
+++ src/main/resources/server_template.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+# template properites:
+# server_name: the instance name of the server process,should be unique in all machines.
+# server_class_path: the class path of the server process,may be empty
+# server_main_class: the main class of the server process,must be set
+# server_args: the arguments of of the server process,may be empty
+
+###########################################
+########replaced parameters################
+###########################################
+server_args="{{server_args}}"
+server_jvm_args="{{server_jvm_args}}"
+server_name="{{server_name}}"
+server_main_class="{{server_main_class}}"
+server_home="{{server_home}}"
+server_log_home="{{server_log_home}}"
+server_resources="{{server_home}}/resources/"
+server_class_path="";
+###########################################
+
+get_server_pids() {
+    pids=( `ps -eo pid,user,cmd | grep "${server_home}" | grep "${server_main_class}" | awk '{print $1}'` )
+}
+
+set_high_priority_jar() {
+    #find the jars
+    jars=( `find ${server_home}/lib/ -name $1* | sort -fr` )
+    if [ "${#jars[@]}" -gt 0 ]; then
+        #exist logback-classic jar
+        echo "find ${jars[0]}, set it to the java classpath"
+        server_class_path=$server_class_path:${jars[0]}
+    fi
+}
+
+start_server() {
+    echo "starting server ${server_name}"
+    #all server pids
+    get_server_pids
+    if [ ${#pids[*]} -gt 0 ]; then
+        echo "${server_name} is started,the pid is ${pids[@]}"
+        exit 1
+    fi
+
+    echo ""
+    echo "#########server configs################################################################################"
+    echo "  server_args=${server_args}"
+    echo "  server_jvm_args=${server_jvm_args}"
+    echo "  server_name=${server_name}"
+    echo "  server_main_class=${server_main_class}"
+    echo "  server_home=${server_home}"
+    echo "  server_log_home=${server_log_home}"
+    echo "  server_resources=${server_resources}"
+    echo "#########server configs################################################################################"
+    echo ""
+
+    #set logback-classic jar
+    set_high_priority_jar logback-classic
+
+    #log4j-over-slf4j jar
+    set_high_priority_jar log4j-over-slf4j
+
+    #jul-to-slf4j jar
+    set_high_priority_jar jul-to-slf4j
+
+    #log4j-over-slf4j jar
+    set_high_priority_jar jcl-over-slf4j
+
+
+    #iterator server jar libs
+    for i in `ls ${server_home}/lib/ | sort -rf`
+    do
+        server_class_path=$server_class_path:${server_home}/lib/$i
+    done
+    export CLASSPATH=${server_class_path}
+    #server start time
+    start_time=`date "+%Y-%m-%d %H:%M:%S"`
+
+    #start the server
+    java ${server_jvm_args} -Dstart_time="${start_time}" -Dserver_home=${server_home} -Dserver_log_home=${server_log_home} -Dserver_resources=${server_resources} -Dserver_name=${server_name} ${system_props} ${server_main_class} ${server_args} >> ${server_log_home}/${server_name}.log 2>&1 &
+    sleep 3
+    get_server_pids
+    if [ ${#pids[*]} -gt 1 ]; then
+        echo "${server_name} is start success,but find multiple instances pids: ${pids[@]}"
+        exit 1
+    elif [ ${#pids[*]} -eq 1 ]; then
+        echo "${server_name} is start success,the pid is ${pids[@]}"
+    else
+        echo "Fail,see log ${server_log_home}/${server_name}.log"
+        exit 1
+    fi
+}
+
+stop_server() {
+    echo "stoping server ${server_name}"
+    #all pids
+    get_server_pids
+    if [ ${#pids[*]} -gt 1 ]; then #multiple server
+        echo "find multiple ${server_name} proccess, infos:"
+        ps -eo pid,user,cmd | grep "${server_resources}" | grep "${server_main_class}"
+        echo "stop server fail,please manual processing"
+        exit 1
+    elif [ ${#pids[*]} -eq 1 ]; then
+        echo "kill ${pids[@]}"
+        kill ${pids[@]}
+        for (( i = 0; i < 5; i ++ ))
+        do
+            sleep 1
+            get_server_pids
+            if [ ${#pids[*]} -le 0 ]; then
+                break
+            fi
+        done
+        if [ ${#pids[*]} -gt 0 ]; then
+            echo "stop server fail,please manual processing"
+            exit 1
+        else
+            echo "stop server success"
+        fi
+    else
+        echo "server ${server_name} is not exist"
+    fi
+}
+
+case "$1" in
+    start)
+        start_server
+    ;;
+    stop)
+        stop_server
+    ;;
+    restart)
+        stop_server
+        start_server
+    ;;
+    *)
+        echo "Usage ${0} <start|stop|restart>"
+        exit 1
+    ;;
+esac
+exit 0
